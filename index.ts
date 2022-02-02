9ba873f763f9986b5eebe608e918c6cf3bb6a1c4
SieR-VR <nwh63@naver.com>
2022-02-02 11:32:52 +0900

    push

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import Express from "express";
 import Prism from "prism-vrc";
+import * as fs from "fs";
 
 const app = Express();
-app.get('/', async (req, res) => {
+app.get('*', async (req, res) => {
+    console.log(req.url);
+
     res.writeHead(200, { 'Content-Type': 'video/mp4' });
-    res.end(await Prism("Hello World"));
+    res.end(await Prism(fs.readFileSync("./assets/test.txt", "utf8"), {
+        secondsPerFrame: 5,
+        bitHeight: 5,
+        bitWidth: 5,
+        quality: 100,
+        height: 720,
+        width: 1280,
+    }));
 });
 
 app.listen(8080, () => {
